73d0c2a399acc9f5eb38bb1e5f768699d9e6ac6d
Victor Tolbert <victor.tolbert@gmail.com>
2017-02-06 09:40:35 -0500

    Add privacy and terms

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,3 +1,43 @@
 <template lang="pug">
-  .privacy Privacy
+  .page.privacy
+    ui-title Privacy Policy
+    p
+      b General:
+      br
+      | Videa, LLC (hereinafter referred to as “Videa”) respects the privacy of visitors to the Site and its Visitors, as defined in the Visitor Agreement. Please read this Privacy Policy carefully so that Visitor understands Videa's privacy practices. Questions regarding privacy issues should be directed to Videa, LLC, 3390 Peachtree Road NE, Suite 700 Atlanta, Georgia 30326.
+    p
+      | All capitalized terms shall have the meaning given to them in the Visitor Agreement.
+    p
+      b The Types of Information Videa Collects:
+      br
+      u Anonymous User Data.
+      |  Videa's web servers automatically collect and store certain limited information about Visitor's computer's connection to the Internet, including Visitor's IP address, when Visitor visits or browses the Site. (Visitor's IP address is a number that lets computers attached to the Internet know where to send Visitor data -- such as the web pages Visitor views.) Visitor's IP address does not identify Visitor personally to us. Videa uses this information to deliver Videa's Site to Visitor upon request, to measure traffic within the Site, and to tailor specific information to certain general classes of IP addresses.
+    p
+      u Personally-Identifying Information
+      | . Visitor does not have to disclose any personally-identifying information (such as Visitor's name or email address) to browse the Site. The only personally-identifying information that Videa gathers through the Site is information that Visitor voluntarily provide to us. However, in order to create and Account, Visitor must provide personally-identifying information as part of the registration process.  If Visitor creates an Account and provides Videa with Visitor's name, email address, phone number, business address, and/or other personally-identifying information when Visitor uses the Site to create an Account, Videa may use Visitor's personally-identifying information to send Visitor a response and Videa may, but is not obligated, to store such personally identifying information and keep a record of Visitor's  personally identifying information.  If Visitor uses the Site to direct an inquiry or request to one of Videa's affiliates, Videa will forward Visitor's request and related personally-identifying information to the appropriate affiliate so that Videa's affiliate can respond to Visitor's inquiry. Videa's affiliates' use of the personally-identifying information collected through the Site will be governed by this privacy policy.  If Visitor inadvertently submits personally identifying information to this Site, Videa is not responsible for deleting or refusing to accept any such personally identifying information, however, such inadvertent submissions shall receive all the privacy protections contained herein.
+    p
+      b Use of Data:
+      br
+      u Videa's Use of Personally-Identifying Information
+      | :
+      br
+      | Videa will not disclose personally-identifying information collected through the Site to third parties except as provided in this privacy policy. Videa reserves the right to release personally-identifying information about the Site visitors when Videa believes that such disclosure is necessary or appropriate to comply with law or to protect the safety or property of Videa, visitors to the Site, the public, Videa's affiliates and/or Videa's personnel. Videa also reserves the right to disclose personally-identifying information about the Site's visitors to third-party contractors Videa engages to assist Videa with the operation of the Site or other aspects of Videa's business when Videa's contractors need access to such information to perform the services Videa requests.  Further, Videa reserves the right to disclose personally-identifying information to any affiliates of Videa if necessary. In the event that Videa merges with another entity or otherwise transfers substantially all of its assets to another entity, information collected from the Site visitors would be among the transferred assets.  Use of such personally identifying information shall be limited and shall adhere to will comply with all applicable laws, regulations, and rules (including without limitation industry standard best practice standards.  Videa will retain and use Visitor's information as necessary to comply with Videa's legal obligations, resolve disputes, and enforce Videa's agreements.
+    p
+      b Miscellaneous
+      br
+      u Links to Other Websites
+      | :
+      br
+      | The Site may include links to other websites operated by third parties whose privacy policies Videa does not control, provided, however, that links to any websites of affiliates of Videa shall be governed by the privacy policy. Videa has no responsibility or control for the content or information collection practices of, or for any potential damage arising out of or in connection with any of these Web sites to which it is linked.  When Visitor accesses another website through the Site, any information Visitor provides is governed by the privacy policy of the operator of the site Visitor is visiting.  These links are included for Visitor's convenience and inclusion of any link does not imply endorsement, sponsorship, or recommendation of Videa of the site.  Videa encourages Visitor to review the privacy policy of any third-party website Visitor accesses through the Site.
+    p
+      u Changes to this Privacy Policy
+      | :
+      br
+      | A final note: The Web is an evolving medium. Videa may update Videa's Privacy Policy at any time. By visiting Videa's site, Visitor agrees that Visitor's visit, and any dispute over Videa's privacy practices, is governed by this Privacy Policy and Videa's Visitor Agreement. Any such revisions will be effective on the date of posting to the site. Visitor should periodically visit this page to review the current terms to which Visitor is bound.
+    p
+      u Site Security
+      | :
+      br
+      | Videa takes commercially reasonable security precautions in order to keep the information disclosed to this Site secure. However, notwithstanding such precautions, Videa assumes no responsibility for the security of this Site or any data transmitted to Videa, including, but not limited to, information related to potential employment or applications therefore, or any other matter.
+
 </template>
