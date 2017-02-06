b667ca3022a6e4c5b954e1c3178a360685e167cb
Victor Tolbert <victor.tolbert@gmail.com>
2017-02-06 12:55:19 -0500

    Remove comments and update datagrid-heading component

--- src/components/common/datagrid/datagrid-heading/index.vue
+++ src/components/common/datagrid/datagrid-heading/index.vue
@@ -1,28 +1,11 @@
 <template lang="pug">
   .vui-grid.vui-grid--align-spread.vui-m-bottom--xx-small
-    //- #viewFormatSelector
-    //-   .vui-hint-parent.hint-parent-demo
-    //-     | Select a view format
-    //-     button.vui-button.vui-button--icon-border-filled.vui-m-left--x-small
-    //-       icon(name="th", style='color: hsla(304, 48%, 28%, 1)')
-    //-       span.vui-assistive-text Show Table View
-    //-     button.vui-button.vui-button--icon-border-filled.vui-m-left--x-small
-    //-       icon(name="pie-chart", style='color: hsla(304, 48%, 28%, 1)')
-    //-       span.vui-assistive-text Show Chart View
-    #excelExporter.vui-hint-parent.hint-parent-demo.vui-align-middle.vui-container--right
+    .exporter.vui-hint-parent.hint-parent-demo.vui-align-middle.vui-container--right
       | Export to Excel
       button.vui-button.vui-button--icon-border-filled.vui-m-left--x-small
-        icon(name='download', style='width: 1.5rem;height: 1.5rem;color:#0177a2')
+        vui-icon(
+          name = 'download'
+          style = 'width: 1.5rem;height: 1.5rem; color: #0177a2'
+        )
       span.vui-assistive-text Export to Excel
 </template>
-
-<script>
-  export default {
-    props: [ 'heading' ],
-
-    data () {
-      return {
-      }
-    }
-  }
-</script>
