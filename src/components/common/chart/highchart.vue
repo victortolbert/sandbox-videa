5983adb7b4b017fdef9b2c7d41740a59ee923f42
Victor Tolbert <victor.tolbert@gmail.com>
2017-02-07 11:51:37 -0500

    Add highcharts package and create an example component and page

---
+++ src/components/common/chart/highchart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div><div>
+</template>
+
+<script>
+var Highcharts = require('highcharts');
+export default {
+    name : "Chart",
+    props : {
+      series : {
+        type: Array,
+        required: true
+      }
+  },
+    data : function() {
+      return {
+        target: undefined
+      }
+    },
+    mounted : function() {
+      this.target = Highcharts.chart(this.$el, {
+        title: {
+          text: 'Monthly Average Temperature',
+          x: -20 // center
+        },
+        subtitle: {
+          text: 'Source: WorldClimate.com',
+          x: -20
+        },
+        xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+          title: {
+          text: 'Temperature (°C)'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        valueSuffix: '°C'
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+            borderWidth: 0
+      },
+      series: this.series
+    });
+  },
+  beforeDestroy: function() {
+    this.target.destroy();
+  },
+}
+</script>
---
+++ src/router/example-routes.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    path: '/examples/charts/highchart-example',
+    name: 'hightchart-example',
+    component: require('pages/examples/charts/highchart-example')
+  }
+
+]
