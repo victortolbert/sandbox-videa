6ca52f1d6a24b5b973df2c4b8de19ee022da507c
Victor Tolbert <victor.tolbert@gmail.com>
2017-02-10 08:52:36 -0500

    Use series data from campaigns dummy data

--- src/components/campaign-performance/campaign-performance-details-daypart-summary-chart.vue
+++ src/components/campaign-performance/campaign-performance-details-daypart-summary-chart.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
   .daypart-summary-chart(v-if='campaign.daypartSummary')
     chart(
-      v-bind:series = 'initSeries'
+      v-bind:series = 'campaign.series'
     )
 </template>
 
 <script>
-
   import Chart from '~components/common/chart/highchart'
 
   export default {
@@ -16,26 +15,6 @@
       }
     },
     name: 'campaign-performance-datails-daypart-summary-chart',
-    components: { Chart },
-    computed: {
-      initSeries () {
-        return [
-          {
-            name: 'Purchased To Date',
-            data: [52, 18, 35, 29, 11],
-            color: '#6CB9BD'
-          }, {
-            name: 'Delivered To Date',
-            data: [70, 45, 25, 20, 20],
-            color: '#80D9BA'
-          },
-          {
-            name: '+/-',
-            data: [-20, -10, -19, -20, -25],
-            color: '#533873'
-          }
-        ]
-      }
-    }
+    components: { Chart }
   }
 </script>
